b15d0bc6f3f9ae71d6b56b8d989c7950fd5a1bc9
CarlosMohr <carlosgmohr03@gmail.com>
2026-08-10 21:54:31 -0300

    feat: estrutura inicial de POO organizada por aulas

---
+++ Exercicio1/Aluno.java
@@ -0,0 +1,13 @@
+package Exercicio1;
+
+public class Aluno {
+    String nome;
+    int idade;
+    String curso;
+
+
+    void apresentarDados() {
+        System.out.println("Nome do aluno: " + nome + "\nIdade do aluno: " + idade + "\nCurso do aluno: " + curso);
+    }
+    
+}
