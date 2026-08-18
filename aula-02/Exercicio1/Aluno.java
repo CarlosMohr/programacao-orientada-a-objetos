feb996a5bcd81d448f3989407b64a8bf72845bde
CarlosMohr <carlosgmohr03@gmail.com>
2026-08-18 00:56:30 -0300

    Commit dos exercicios da aula 3

--- aula-02/Exercicio1/Aluno.java
+++ aula-02/Exercicio1/Aluno.java
@@ -1,4 +1,4 @@
-package Exercicio1;
+package aula02.Exercicio1;
 
 public class Aluno {
     String nome;
@@ -8,6 +8,6 @@ public class Aluno {
 
     void apresentarDados() {
         System.out.println("Nome do aluno: " + nome + "\nIdade do aluno: " + idade + "\nCurso do aluno: " + curso);
-    }
+    }P
     
 }
